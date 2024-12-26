da0c9e40d79ffb69f765ddbea13a1a634aecca7d
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2024-12-27 03:58:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,69 @@
-//complete this code
+// //complete this code
+// class Rectangle {
+// 	constructor(width, height){
+// 		this._width=width;
+// 		this._height=height;
+// 	}
+
+// 	get width(){
+// 		return this._width;
+// 	}
+// 	get height(){
+// 		return this._height;
+// 	}
+
+// 	getArea(){
+// 		let area=this.height*this.width;
+// 		return area;
+// 	}
+// }
+
+// class Square extends Rectangle {
+// 	constructor(side){
+// 		this._side=side;
+// 	}
+
+// 	getPerimeter(){
+// 		let per=4*this.side;
+// 		return per;
+// 	}
+// }
+
 class Rectangle {
-	constructor(width, height){
-		this._width=width;
-		this._height=height;
-	}
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    }
 
-	get width(){
-		return this._width;
-	}
-	get height(){
-		return this._height;
-	}
+    get width() {
+        return this._width;
+    }
 
-	getArea(){
-		let area=this.height*this.width;
-		return area;
-	}
+    get height() {
+        return this._height;
+    }
+
+    getArea() {
+        let area = this.height * this.width;
+        return area;
+    }
 }
 
 class Square extends Rectangle {
-	constructor(side){
-		this._side=side;
-	}
+    constructor(side) {
+        // Call the parent class constructor with width and height being the same
+        super(side, side);
+    }
 
-	getPerimeter(){
-		let per=4*this.side;
-		return per;
-	}
+    get side() {
+        return this.width; // Or this.height, since they are the same for a square
+    }
+
+    getPerimeter() {
+        let per = 4 * this.side;
+        return per;
+    }
 }
-
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
